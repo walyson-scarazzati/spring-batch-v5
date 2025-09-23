00161cfea265af7739bf403cd595465785b45d6c
Walyson Scarazzati da Silva <53382989+walyson-scarazzati@users.noreply.github.com>
2025-09-23 22:02:42 +0100

    Example Spring Batch V5

---
+++ src/main/java/br/com/walyson/spring_batch_v5/SpringBatchV5Application.java
@@ -0,0 +1,13 @@
+package br.com.walyson.spring_batch_v5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchV5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchV5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/walyson/spring_batch_v5/SpringBatchV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.walyson.spring_batch_v5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchV5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
